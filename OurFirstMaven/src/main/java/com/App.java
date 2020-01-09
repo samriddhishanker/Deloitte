8ac12172a800b7584c3dd5aa675608f6a6efd80c
samriddhishanker <59157181+samriddhishanker@users.noreply.github.com>
2020-01-09 16:43:17 +0530

    Add files via upload

---
+++ OurFirstMaven/src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws ClassNotFoundException
+    {
+    	Class.forName("com.mysql.jdbc.Driver");
+        System.out.println( "Driver Loaded" );
+    }
+}
